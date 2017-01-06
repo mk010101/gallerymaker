8e34010645c405cbe6ab928b3b608084529bf9e7
Max Kostin <mkostin@videotel.com>
2017-01-06 11:42:55 +0000

    build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const fs = require("fs-extra");
 const sharp = require("sharp");
 
-let btnOpen;
+let fileOpen;
+let thumbSizeSlider;
+let numFilesEl;
+
+let thumbSize = "150";
 
 let pathSrc;
 let pathPreview;
@@ -14,7 +18,9 @@ exports.init = function() {
 
     let features = ["face", "side", "34", "other"];
 
-    btnOpen = document.getElementById("open");
+    fileOpen = document.getElementById("open");
+    thumbSizeSlider = document.getElementById("thumb-size");
+    numFilesEl = document.getElementById("num-files");
 
     let str = "";
 
@@ -30,9 +36,21 @@ exports.init = function() {
 };
 
 function setListeners() {
-    btnOpen.addEventListener("change", onDirSelect);
+    document.getElementById("btnOpen").addEventListener("click", onOpenDir);
+    fileOpen.addEventListener("change", onDirSelect);
+    thumbSizeSlider.addEventListener("change", onThumbSizeChange);
+
 }
 
+
+function onOpenDir() {
+    fileOpen.click();
+}
+
+function onThumbSizeChange() {
+    thumbSize = thumbSizeSlider.value;
+    buildView();
+}
 
 function onDirSelect(e) {
 
@@ -51,6 +69,8 @@ function onDirSelect(e) {
 
     }
 
+    numFilesEl.innerHTML = filesArr.length + "";
+
     buildView();
 
 }
@@ -68,7 +88,9 @@ function parseDirectory(dir, dirName) {
             filesArr.push({
                 folder: dirName,
                 fileName: files[i],
-                path: f
+                path: f,
+                marked: false,
+                rotation: ""
             });
         }
 
@@ -83,7 +105,18 @@ function buildView() {
     for (let i = 0; i < filesArr.length; i++) {
         let obj = filesArr[i];
         let f = "file:///" + obj.path;
-        str += `<div class="item"><img src="${f}" data-folder="${obj.folder}"></div>`;
+        let marked = obj.marked? "marked" : "";
+        //str += `<div class="item"><img src="${f}" data-folder="${obj.folder}"></div>`;
+        str += `<div class="item ${marked}" 
+                    data-id="${i}" 
+                    style="background: url('${f}'); 
+                    background-size: contain; 
+                    background-repeat: no-repeat; 
+                    background-position: center;
+                    width: ${thumbSize}px;
+                    height: ${thumbSize}px;
+                    ">
+                </div>`;
     }
 
     document.getElementById("view").innerHTML = str;
